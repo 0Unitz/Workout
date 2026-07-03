a3053d3211be287969e1d8038911e15fe2990b41
unknown <epyon404@gmail.com>
2026-07-03 17:08:32 -0400

    RPME: bump sw cache rpme-v17 -> rpme-v18 so installed PWAs pick up Phase 1 program

--- rpg/sw.js
+++ rpg/sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'rpme-v17';
+const CACHE = 'rpme-v18';
 const FILES = ['./index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', e => {
