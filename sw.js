853dfc604fa70b130a9e548726167b6d16c95671
0Unitz <272057289+0Unitz@users.noreply.github.com>
2026-07-19 19:34:23 -0400

    Add privacy policy page, fix manifest (id/categories/separate maskable icon) (sw rpme-v22)

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'rpme-v21';
+const CACHE = 'rpme-v22';
 const FILES = ['./index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', e => {
